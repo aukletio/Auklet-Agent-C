8de5980cb78071afcf3a88b726fa599265bea686
Karl Schultheisz <karl.schultheisz@esg-usa.com>
2018-03-29 12:18:00 -0400

    config.mk: don't override env-supplied values

--- src/config.mk
+++ src/config.mk
@@ -2,8 +2,8 @@
 CFLAGS = -Wall -Werror -std=c99 -pedantic -D_POSIX_C_SOURCE=200809L -g
 
 # default toolchain
-OC = objcopy
-NM = nm
+OC ?= objcopy
+NM ?= nm
 
 # agent library installation path
 INSTALL = /usr/local/lib

a7609d28f7cebda1b5b0142b29de9d70f29b38e5
Karl Schultheisz <karl.schultheisz@esg-usa.com>
2018-03-29 11:42:21 -0400

    config.mk: add OC and NM default values

--- config.mk
+++ config.mk
@@ -1,6 +1,10 @@
 # flags
 CFLAGS = -Wall -Werror -std=c99 -pedantic -D_POSIX_C_SOURCE=200809L -g
 
+# default toolchain
+OC = objcopy
+NM = nm
+
 # agent library installation path
 INSTALL = /usr/local/lib
 

83f957ad47830c569cc22bbe063702d8a54391a1
Ron Jenkins Jr <ronald.j.jenkins@gmail.com>
2018-11-16 00:47:19 -0500

    Implement unified S3 bucket structure

--- .devops/deploy.sh
+++ .devops/deploy.sh
@@ -4,17 +4,15 @@ if [[ "$1" == "" ]]; then
   echo "ERROR: env not provided."
   exit 1
 fi
-ENVDIR=$1
+TARGET_ENV=$1
 VERSION="$(cat VERSION)"
-VERSION_SIMPLE=$(cat VERSION | xargs | cut -f1 -d"+")
 export TIMESTAMP="$(date --rfc-3339=seconds | sed 's/ /T/')"
 
 echo 'Compiling/packaging C agent binaries...'
 echo
 PREFIX='libauklet'
 BENCH_PREFIX="$PREFIX-bench-overhead"
-S3_BUCKET='auklet'
-S3_PREFIX='agent/c'
+S3_PREFIX='auklet/c/agent'
 while IFS=, read arch cc ar ld oc nm pkg
 do
   echo "=== $arch ==="
@@ -33,44 +31,20 @@ done < arch-grid.csv
 echo 'Installing AWS CLI...'
 sudo apt -y install awscli > /dev/null 2>&1
 
-if [[ "$ENVDIR" == "release" ]]; then
-  echo 'Erasing production C agent binaries in public S3...'
-  aws s3 rm s3://$S3_BUCKET/$S3_PREFIX/latest/ --recursive
+if [[ "$TARGET_ENV" == "release" ]]; then
+  echo 'Erasing production C agent binaries in S3...'
+  aws s3 rm s3://$S3_PREFIX/latest/ --recursive
 fi
 
 echo 'Uploading C agent binaries to S3...'
-# Decide which S3 subdir to put the binaries in.
-case "$ENVDIR" in
-  beta)
-    S3_ENVDIR='staging'
-    ;;
-  rc)
-    S3_ENVDIR='qa'
-    ;;
-  release)
-    S3_ENVDIR='production'
-    ;;
-  *)
-    echo "ERROR: invalid ENVDIR '$ENVDIR' provided."
-    exit 1
-    ;;
-esac
 # Iterate over each file and upload it to S3.
 for f in ${PREFIX}-*; do
   # Upload to the internal bucket.
-  S3_LOCATION="s3://auklet-profiler/$S3_ENVDIR/$S3_PREFIX/$VERSION/$f"
+  S3_LOCATION="s3://$S3_PREFIX/$VERSION/$f"
   aws s3 cp $f $S3_LOCATION
-  # Do not upload the benchmark tool to the public bucket.
-  if [[ $f == ${BENCH_PREFIX}* ]]; then
-    continue;
-  fi
-  # Upload to the public bucket for production builds.
-  if [[ "$ENVDIR" == "release" ]]; then
-    # Copy to the public versioned directory.
-    VERSIONED_NAME="${f/$VERSION/$VERSION_SIMPLE}"
-    aws s3 cp $S3_LOCATION s3://$S3_BUCKET/$S3_PREFIX/$VERSION_SIMPLE/$VERSIONED_NAME
-    # Copy to the public "latest" directory.
+  # Copy to the "latest" dir for production builds.
+  if [[ "$TARGET_ENV" == "release" ]]; then
     LATEST_NAME="${f/$VERSION/latest}"
-    aws s3 cp $S3_LOCATION s3://$S3_BUCKET/$S3_PREFIX/latest/$LATEST_NAME
+    aws s3 cp $S3_LOCATION s3://$S3_PREFIX/latest/$LATEST_NAME
   fi
 done

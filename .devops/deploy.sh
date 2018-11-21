3e679068e7c2470a64841aff57b08ce2a92b0eae
Ron Jenkins Jr <ronald.j.jenkins@gmail.com>
2018-11-20 21:26:13 -0500

    Fix versioning during deployment

--- .devops/deploy.sh
+++ .devops/deploy.sh
@@ -5,8 +5,8 @@ if [[ "$1" == "" ]]; then
   exit 1
 fi
 TARGET_ENV=$1
-VERSION="$(cat VERSION)"
-VERSION_SIMPLE=$(cat VERSION | xargs | cut -f1 -d"+")
+VERSION="$(cat ~/.version)"
+VERSION_SIMPLE="$(cat ~/.version | xargs | cut -f1 -d"+")"
 export TIMESTAMP="$(date --rfc-3339=seconds | sed 's/ /T/')"
 
 echo 'Compiling/packaging C agent binaries...'
